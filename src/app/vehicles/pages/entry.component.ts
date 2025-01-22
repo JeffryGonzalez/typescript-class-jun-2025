dada32591dcdad537e0453de07833eaec1d8ee2c
Jeff Gonzalez <jeff@hypertheory.com>
2025-01-22 14:22:15 -0500

    entry overview

--- src/app/vehicles/pages/entry.component.ts
+++ src/app/vehicles/pages/entry.component.ts
@@ -1,10 +1,16 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { VehicleStore } from '../services/vehicle-store';
 
 @Component({
   selector: 'app-vehicle-entry',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
-  template: ` <p>Vehicle Entry</p> `,
+  template: `
+    <p>Vehicle Entry</p>
+    <p>You have {{ store.entities().length }} vehicles. Add Some More?</p>
+  `,
   styles: ``,
 })
-export class EntryComponent {}
+export class EntryComponent {
+  store = inject(VehicleStore);
+}
